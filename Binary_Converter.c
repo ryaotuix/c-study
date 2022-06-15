e4353666ba23a54bcf7fa7a6d54919e8b5868251
ryaotuix <ryaotuix@naver.com>
2022-06-15 17:27:38 +0900

    continue tmr

---
+++ Binary_Converter.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* C accepts 8 and 16 진수! */
+int main(void){
+  int num1 = 10;
+  int num2 = 0x4; // starts with 0x it's 16진수
+  int num3 = 012; // starts with 0, it's 8진수
+}
+
+
+
+
+
+
+/* Change into binary and 16진수 */
+
+// int void(main){
+//   int num;
+//   scanf("%d", &num);
+//
+// }
