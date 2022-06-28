3b6002bce54add277d67299b0d8700c0b81dc462
ryaotuix <ryaotuix@naver.com>
2022-06-28 16:51:27 +0900

    we cannot do char * str = NULL; scanf("%s", str); directly

--- Let's do it!/task2-4.c
+++ Let's do it!/task2-4.c
@@ -4,7 +4,8 @@
 int IsPalindrome(char * str, int len)
 {
   int front = 0, end = len-1;
-  for(int i=0; i<len; i++){
+  for(int i=0; i<len/2; i++){
+    // printf("%c and %c\n", str[front], str[end]); to check
     if(str[front] == *(str+end)){
       front++;
       end--;
@@ -18,13 +19,19 @@ int IsPalindrome(char * str, int len)
 int main(void)
 {
   char * str = 0;
+  // The following solves the problem that we cannot do scanf("%s", str); directly
+  char string[100];
   printf("Type a word: ");
-  scanf("%s", str);
-  fflush(stdin);
-  
-  int len = sizeof(str) / sizeof(char);
+  scanf("%s", string);
+  str = string;
+
+  int len = 0;
+  while(*(str+len) != '\0'){
+    len++;
+  }
+
   int pal = IsPalindrome(str, len);
-  printf("%d", pal);
+
 
   if (pal == 1)
     printf("It is a palindrome!\n");
